e37937fa988953b8e9b62a46b2671e90508c21b8
Tobias Foit <tobias.foit@gmail.com>
2023-11-02 18:54:53 +0000

    all the APIs from the server!!

--- server.js
+++ server.js
@@ -10,9 +10,10 @@ require("dotenv").config();
 const axios = require("axios");
 
 // tell the server which port to run on. We can set this in our .env but we dont have to
-const PORT = 10000;
+const PORT = 8001;
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
 const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
+const LOCATION_API_KEY = process.env.LOCATION_API_KEY;
 
 // instantiating our instance of express - we are calling it app (app is now a giant object with all the HTTP methods)
 const app = express();
@@ -20,57 +21,47 @@ const app = express();
 // ACTIVATE CORS - dont forget the brackets
 app.use(cors());
 
-// import our json data
-// const data = require("./data/weather.json");
+app.get("/", (request, response) => {
+  response.json("whoop whoop");
+});
 
-// Use the .find() method to discover which city the `lat`, `lon` and `searchQuery` information belong to. If the user did not search for one of the three cities that we have information about (Seattle, Paris, or Amman), return an error.
-// async function findCity(lat, lon) {
-//   const API = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
-//   const data = await axios.get(API);
-//   const foundCity = data.find(
-//     (cityData) => cityData.lat == lat && cityData.lon == lon
-//   );
-//   return foundCity;
-// }
-
-async function findCity(lat, lon) {
-  const API = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
-  const weatherData = await axios.get(API);
-  return weatherData.data;
+async function findLocation(cityname) {
+  const locationAPI = `https://eu1.locationiq.com/v1/search?q=${cityname}&key=${LOCATION_API_KEY}&format=json`;
+  const locationData = await axios.get(locationAPI);
+  return locationData;
 }
 
 async function findMovies(cityname) {
   const moviesAPI = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${cityname}`;
   const movieData = await axios.get(moviesAPI);
-
   return movieData;
 }
 
-// Create an API endpoint of `/weather` that processes a `GET` request that contains `lat`, `lon` and `searchQuery` information.
-app.get("/", (request, response) => {
-  response.json("whoop whoop");
-});
-//UNCOMMENT THIS TO SHOW WEATHER AGAIN!
-app.get("/weather", async (request, response) => {
-  const weatherRes = await findCity(request.query.lat, request.query.lon);
-  response.json(weatherRes);
-});
+async function findWeather(lat, lon) {
+  const API = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
+  const weatherData = await axios.get(API);
+  return weatherData.data;
+}
 
-app.get("/movies", async (request, response) => {
-  const movieRes = await findMovies(request.query.city);
+app.get("/request", async (request, response) => {
+  try {
+    const locationRes = await findLocation(request.query.q);
+    const movieRes = await findMovies(request.query.q);
 
-  const movieDataWrangled = movieRes.data.results.map((result) => {
-    return {
-      id: result.title + "_" + result.release_date,
-      title: result.title,
-      overview: result.overview,
-      image_url: `https://image.tmdb.org/t/p/w500/` + result.poster_path,
-      release_date: result.release_date,
+    // wrangle the data so that it has three objects: location, movie, weather
+    const wrangledData = {
+      lon: locationRes.data[0].lon,
+      lat: locationRes.data[0].lat,
+      location: locationRes.data[0].display_name,
+      movie: movieRes.data.results[0].original_title,
+      movieImg: movieRes.data.results[0].poster_path,
     };
-  });
 
-  response.json(movieDataWrangled[0]);
+    response.json(wrangledData);
+  } catch (error) {
+    console.error("Error in /request: ", error);
+    response.status(500).json({ error: "An error occurred" });
+  }
 });
 
-// start the server on our PORT, and give it a console.log to see it is working
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
