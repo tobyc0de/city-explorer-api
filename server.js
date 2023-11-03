9e912ba47bca025895128f2e7e940d268d533495
Tobias Foit <tobias.foit@gmail.com>
2023-11-03 08:30:37 +0000

    added weather

--- server.js
+++ server.js
@@ -21,10 +21,6 @@ const app = express();
 // ACTIVATE CORS - dont forget the brackets
 app.use(cors());
 
-app.get("/", (request, response) => {
-  response.json("whoop whoop");
-});
-
 async function findLocation(cityname) {
   const locationAPI = `https://eu1.locationiq.com/v1/search?q=${cityname}&key=${LOCATION_API_KEY}&format=json`;
   const locationData = await axios.get(locationAPI);
@@ -40,13 +36,21 @@ async function findMovies(cityname) {
 async function findWeather(lat, lon) {
   const API = `http://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lon}`;
   const weatherData = await axios.get(API);
-  return weatherData.data;
+  return weatherData;
 }
+
+app.get("/", (request, response) => {
+  response.json("whoop whoop");
+});
 
 app.get("/request", async (request, response) => {
   try {
     const locationRes = await findLocation(request.query.q);
     const movieRes = await findMovies(request.query.q);
+    const weatherRes = await findWeather(
+      locationRes.data[0].lat,
+      locationRes.data[0].lon
+    );
 
     // wrangle the data so that it has three objects: location, movie, weather
     const wrangledData = {
@@ -55,6 +59,7 @@ app.get("/request", async (request, response) => {
       location: locationRes.data[0].display_name,
       movie: movieRes.data.results[0].original_title,
       movieImg: movieRes.data.results[0].poster_path,
+      weather: weatherRes.data,
     };
 
     response.json(wrangledData);
